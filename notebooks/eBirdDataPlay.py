60c6c4c4b0d1e970e0d897e45a1b7d235edacfda
jimbo.19 <38577751+jimchurches@users.noreply.github.com>
2025-06-04 10:02:37 +1000

    Removed some rubbish from file.

--- notebooks/eBirdDataPlay.py
+++ notebooks/eBirdDataPlay.py
@@ -13,23 +13,6 @@
 #     name: python3
 # ---
 
-# %%
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.17.2
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# %%
 import os
 import sys
 import pandas as pd
