472b827e17564afb346c6b2364c03339e59ca4f4
jimchurches <jimchurches@>
2025-06-08 08:52:14 +1000

    Added example paths to configuration template to show syntax for macOS and Windows paths

--- scripts/config_template.py
+++ scripts/config_template.py
@@ -1,3 +1,13 @@
 # This file is tracked in Git
 GOOGLE_API_KEY = "REPLACE_WITH_YOUR_KEY"
-DATA_FOLDER = "/path/to/eBird/Data/Folder/"
+DATA_FOLDER = "the/path/to/eBird/Data/Folder/"
+
+## Example path syntax
+##
+## Paths for macOS
+## /Users/bobsmith/OneDrive/ebirdData
+##
+##
+## Path for Windows
+## "C:\\Users\\bobsmith\\Documents\\ebirdData"
+## 
